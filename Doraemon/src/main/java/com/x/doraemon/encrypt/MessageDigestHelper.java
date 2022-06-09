b10b3bc8b44d3859705ad1629caad773675a0760
AD-Wu <944907103@qq.com>
2022-06-09 16:52:12 +0800

    -

--- Doraemon/src/main/java/com/x/doraemon/encrypt/MessageDigestHelper.java
+++ Doraemon/src/main/java/com/x/doraemon/encrypt/MessageDigestHelper.java
@@ -15,25 +15,25 @@ public enum MessageDigestHelper {
     SHA384("SHA-384"),
     SHA512("SHA-512");
 
-    private MessageDigest algorithm;
+    private MessageDigest alg;
 
     private MessageDigestHelper(String name) {
         try {
-            this.algorithm = MessageDigest.getInstance(name);
+            this.alg = MessageDigest.getInstance(name);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
     public MessageDigest algorithm() {
-        return algorithm;
+        return alg;
     }
 
     public byte[] digest(byte[] bs) {
         // 使用指定的字节来更新摘要
-        algorithm.update(bs);
+        alg.update(bs);
         // 完成散列计算，返回计算所得的摘要，并复位算法对象
-        byte[] digest = algorithm.digest();
+        byte[] digest = alg.digest();
         return digest;
     }
 
