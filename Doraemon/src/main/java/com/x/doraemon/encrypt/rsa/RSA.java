ee4447b0ddcde666c74de0a01e3a0034734eff08
AD-Wu <944907103@qq.com>
2022-06-13 12:43:30 +0800

    重命名

--- Doraemon/src/main/java/com/x/doraemon/encrypt/rsa/RSA.java
+++ Doraemon/src/main/java/com/x/doraemon/encrypt/rsa/RSA.java
@@ -14,43 +14,52 @@ import java.security.KeyPairGenerator;
  * @date 2022/6/12 21:54
  */
 public class RSA implements ICipher {
-    
+
     private static final String RSA = Encrypt.Algorithm.RSA.toString();
-    
+
+    /**
+     * 密钥长度
+     * - 默认1024位(128字节)
+     * - 必须是64的倍数
+     * - 范围 512~65536
+     */
+    private final int keySize;
+
     private final Key publicKey;
-    
+
     private final Key privateKey;
-    
+
     /**
      * 构造方法
      * @param keySize 秘钥长度(bit, 512、1024...)
      * @throws Exception
      */
     public RSA(int keySize) throws Exception {
+        this.keySize = keySize;
         KeyPairGenerator pairGen = KeyPairGenerator.getInstance(RSA);
         pairGen.initialize(keySize);
         KeyPair keyPair = pairGen.generateKeyPair();
         this.publicKey = keyPair.getPublic();
         this.privateKey = keyPair.getPrivate();
     }
-    
+
     @Override
     public String algorithm() {
         return Encrypt.Algorithm.RSA.toString();
     }
-    
+
     @Override
     public byte[] encrypt(byte[] bs) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         return cipher.doFinal(bs);
     }
-    
+
     @Override
     public byte[] decrypt(byte[] bs) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.ENCRYPT_MODE, privateKey);
         return cipher.doFinal(bs);
     }
-    
+
 }
